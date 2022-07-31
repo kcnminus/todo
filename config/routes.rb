3a070c01ca66d261ebeecc414551567887811ced
Colin No <colin.no@policygenius.com>
2022-07-31 11:44:31 -0400

    adding complete functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-resources :todo_lists do
-    resources :todo_items
+  resources :todo_lists do
+    resources :todo_items do
+      member do
+        patch :complete 
+      end
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
